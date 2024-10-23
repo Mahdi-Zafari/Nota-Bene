43fadd3d329fdc8b64a77be54803b2ac4b45d0b1
Mahdi-Zafari <143317931+Mahdi-Zafari@users.noreply.github.com>
2024-10-24 01:05:05 +0330

    Added logo and updated site title to 'Nota Bene'
    Logo Added and Title Updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Nota Bene</title>
+
+        <link rel="icon" type="image/png" href="{{ asset('images/app_logo_circle.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
